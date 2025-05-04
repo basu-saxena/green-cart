e343a0d62d48daadce432de61b098b1c75042e73
basu-saxena <basusaxena2003@gmail.com>
2025-05-04 15:36:51 +0530

    bug fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -112,6 +112,7 @@ export const logout = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      path: "/",
     });
     res.json({ success: true, message: "Logged Out !!" });
   } catch (error) {
